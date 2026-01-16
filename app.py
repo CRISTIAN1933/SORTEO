881040c3b75de2650d72d647fb8f3e5687cb16ab
CRISTIAN1933 <109871029+CRISTIAN1933@users.noreply.github.com>
2026-01-16 15:41:31 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ import firebase_admin
 from firebase_admin import credentials, db
 from datetime import datetime
 import re
+import json
 
 # ---------------- FIREBASE INIT ----------------
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+    # Creamos el diccionario de credenciales desde variables de entorno
+    firebase_cred_dict = {
+        "type": "service_account",
+        "project_id": os.environ["FIREBASE_PROJECT_ID"],
+        "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+        "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+        "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+        "client_id": os.environ["FIREBASE_CLIENT_ID"],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]
+    }
 
+    cred = credentials.Certificate(firebase_cred_dict)
     firebase_admin.initialize_app(cred, {
-        "databaseURL": "https://sorteos-para-todos-default-rtdb.firebaseio.com"
+        "databaseURL": os.environ["FIREBASE_DB_URL"]
     })
 
 # ---------------- APP ----------------
